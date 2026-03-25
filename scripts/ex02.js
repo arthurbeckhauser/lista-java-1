09f7e600054161fe3b1b21c014229a88a495118f
arthurbeckhauser <arthur_b_fernandes@estudante.sesisenai.org.br>
2026-03-25 09:51:52 -0300

    add - ex 02 e 03

--- scripts/ex02.js
+++ scripts/ex02.js
@@ -2,6 +2,19 @@
 
 function contagemRegressiva(){
 
+        alert("se prepare para a contagem");
+    for (let i = 10; i >= 0; i--) {
+
+        alert(i);
+
+        console.log(i);
+
+        if (i < 1) {
+            alert("lançamento realizado");
+        }
+
+    }
+
     
 
 }
